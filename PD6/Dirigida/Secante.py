3aa4e5275bf5170837fb15d51378f4153ffda8b0
Cody McCodePants <cody@mccodepants.com>
2022-08-09 18:59:15 -0500

    My message

--- PD6/Dirigida/Secante.py
+++ PD6/Dirigida/Secante.py
@@ -1,10 +1,21 @@
-import numpy as np
-#Fiorella  Meza
-print("Método de la secante")
-eps=1E-5
-maxIter=100
+from cmath import pi
+from numpy import sin,cos,tan
 
-f = lambda x: x**3-3*x+4
+def rad_sex(angle):
+    return (angle/180)*pi
+eps = 1E-5
+maxIter = 100
+l = 2.2
+h = 1.2
+D = 0.75
+beta1= rad_sex(11.5)
+
+A = l*sin(beta1)
+B = l*cos(beta1)
+C = (h+0.5*D)*sin(beta1) - 0.5*D*tan(beta1)
+E = (h+0.5*D)*cos(beta1)-0.5*D
+
+f = lambda x: A*sin(x)*cos(x)+B*sin(x)**2-C*cos(x)-E*sin(x)
 
 x0=1
 x1=2
